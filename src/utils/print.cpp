32663a7050b6704a14aefd9d3324959af29c4110
polodu13 <paul.depetrini@gmail.com>
2026-04-04 00:46:55 +0200

    feat: Enhance messageToClient function with message segmentation but not finish for error Send

--- src/utils/print.cpp
+++ src/utils/print.cpp
@@ -2,25 +2,105 @@
 #include <cstdarg>
 #include <string>
 #include <iostream>
+#include <sys/socket.h>
 
-
-//finir par null dans le text a push
-//ex = messageToServer("tutu","toto",NULL);
-void messageToServer(const char* text, ...)
+// finir par null dans le text a push
+// ex = messageToServer("tutu","toto",NULL);
+void messageToServer(const char *text, ...)
 {
     if (text == NULL)
-        return ;
+        return;
     std::va_list args;
     std::cout << text;
     va_start(args, text);
-    const char* val = va_arg(args, const char*);
+    const char *val = va_arg(args, const char *);
     while (val != NULL)
     {
         std::cout << val;
-        val = va_arg(args, const char*);
+        val = va_arg(args, const char *);
     }
     va_end(args);
     std::cout << std::endl;
 }
 
-void messageToClient(int fdClient, )
+// penser a mettre un @ sur les operateur si cest dans un channel,
+// dabord envoyer le fd a qui envoye, apres celui qui envoie le message
+// ensuite la commande par ex PRVMESSAGE
+// si pas de numericsCode envoyer -1
+// messageToClient(fd, NULL, Kaisss, NULL, PRIVMESSAGE, "Kaissot je te baise", NULL)
+/**
+ * @brief message send to Irc Client FD;
+ * 
+ * @param fdClientReception the IRC client Socket
+ * @param numericsCode The addr of numerics Error Protocol IRC (NULL if not used)
+ * @param sender The name of the sender (Don't forget the @ if operateur of channel)
+ * @param channel The addr of name of the Channel (Don't forget the #) (NULL if not used)
+ * @param command The command ex : JOIN PRVMSG...
+ * @param ... the param and for the penultimate the message to send 
+ * @attention Don't forget the NULL for the end of variadics arguments
+ * @return possible throw if send doesn't worked
+ */
+void messageToClient(int fdClientReception, std::string *numericsCode, std::string &sender, std::string *channel, std::string command, ...)
+{
+    std::string header;
+    std::string msg;
+    header.append(":");
+    header.append(sender + " ");
+    if (numericsCode != NULL)
+        header.append(*numericsCode + " ");
+    header.append(command);
+    if (channel != NULL)
+        header.append(*channel + " ");
+    std::va_list args;
+    va_start(args, command);
+    const char *text = va_arg(args, const char *);
+    const char *nextText;
+    while (text != NULL)
+    {
+        if ((nextText = va_arg(args, const char *)) == NULL)
+        {
+            msg.append(":");
+            msg.append(text);
+        }
+        else
+        {
+            msg.append(text);
+            msg.append(" ");
+        }
+        text = nextText;
+    }
+    msg.append("\r\n");
+    va_end(args);
+    if (msg.length() + header.length() > 510)
+    {
+        size_t posCut = 0;
+        std::string cutMsg;
+        size_t posSpaceInMsg;
+        while (msg.length() + header.length() > 510)
+        {
+            posSpaceInMsg = msg.find_last_of(" ", 300);
+            if (posSpaceInMsg + header.length() > 510)
+            {
+                cutMsg = msg.substr(posCut, 510 - header.length());
+                posCut += 510 - header.length() + 1;
+            }
+            else
+            {
+                cutMsg = msg.substr(posCut, posSpaceInMsg);
+                posCut += posSpaceInMsg + 1;
+            }
+            cutMsg.insert(0, header);
+            send(fdClientReception, (void *)cutMsg.c_str(), cutMsg.length(), MSG_DONTWAIT);
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
+                // TODO: FAIRE MESSAGE ERREUR AVEC THROW
+            }
+        }
+    }
+    else
+        send(fdClientReception, (void *)msg.c_str(), msg.length(), MSG_DONTWAIT);
+    if (errno == EAGAIN || errno == EWOULDBLOCK)
+    {
+        // TODO: FAIRE MESSAGE ERREUR AVEC THROW
+    }
+}
