25d5cd7a82dd16a56feba253afdb20ec4671e9b2
paul de petrini <paul.depetrini@gmail.com>
2026-04-22 01:34:25 +0200

    refactor: Update quitCmd to handle optional explanation message and improve message formatting

--- src/Commands/QUIT.cpp
+++ src/Commands/QUIT.cpp
@@ -1,23 +1,52 @@
 #include "User.hpp"
 
-void User::quitCmd(Server &server)
+void User::quitCmd(Server &server, std::vector<std::string> arg)
 {
-    std::string messageSend = "ERROR :Tu as quitté le serveur fdp\r\n";
+    std::string explainMsg;
+    if (arg.size() > 0)
+    {
+        arg[0].erase(0, 1);
+        explainMsg = arg[0];
+    }
+    std::string messageSend = "ERROR :You left the server";
+    if (explainMsg.empty() == false)
+    {
+        messageSend +=" because you said : " + explainMsg;
+    }
+    messageSend += "\r\n";
+    if (messageSend.size() > MAX_SIZE_MESSAGE)
+    {
+        messageSend.erase(510);
+        messageSend += "\r\n";
+    }
     send(this->_userFd, messageSend.c_str(), messageSend.size(), 0);
     std::set<User *> concernedUsers;
     for (std::map<std::string, Channel*>::iterator it = server._chanMap.begin(); it != server._chanMap.end(); it++)
     {
         if (it->second->checkUser(*this) == true)
         {
-            it->second->deletedUser(*this);
             for (std::vector<User *>::const_iterator it2 = it->second->allUsersInVector().begin(); it2 != it->second->allUsersInVector().end(); ++it2)
-                concernedUsers.insert(*it2);
+            {
+                if ((*it2) != this)
+                    concernedUsers.insert(*it2);
+            }
+            it->second->deletedUser(*this);
+                
         }
     }
-    messageSend = ":" + this->_nickname + "!" + this->_username + " QUIT" + " : Vous pouvez enfin le critiquer, il est enfin parti \r\n";
+    messageSend = ":" + this->_nickname  + "!" + this->_username + "@" + HOST+ " QUIT" + " : Good Bye";
+    if (explainMsg.empty() == false)
+    {
+        messageSend +=" and he said :" + explainMsg;
+    }
+    messageSend += " \r\n";
+    if (messageSend.size() > MAX_SIZE_MESSAGE)
+    {
+        messageSend.erase(510);
+        messageSend += " \r\n";
+    }
     for (std::set<User *>::iterator it = concernedUsers.begin(); it != concernedUsers.end(); it++)
         send((*it)->_userFd, messageSend.c_str(), messageSend.size(), 0);
-
 
     //Kaissot supprime utilisateur du channel
 }
