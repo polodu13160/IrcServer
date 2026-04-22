6079feeaa1f0695fd9ae88276777f7c69a6071ff
Lucas Paysant <lpaysant@z2r10p8.42lyon.fr>
2026-04-22 12:38:11 +0200

    modifs : removed the parsing parts of most functions in cmdPars

--- src/Commands/cmdPars.cpp
+++ src/Commands/cmdPars.cpp
@@ -52,11 +52,6 @@ void cmdPars::handlePrivmsg(Server &server, User &user, std::vector<std::string>
 
 void cmdPars::handleUser(Server &server, User &user, std::vector<std::string> arg)
 {
-	if (arg[0].empty())
-	{
-		std::cout << "There must be at least 1 parameter for this command" << std::endl;
-		return;
-	}
 	user.userCmd(server, arg);
 }
 
@@ -109,26 +104,6 @@ void cmdPars::handleKick(Server &server, User &user, std::vector<std::string> ar
 
 void cmdPars::handleMode(Server &server, User &user, std::vector<std::string> arg)
 {
-	(void)user;
-	(void)server;
-	// MODE (channel/user) (mode : -i, -o...) (param)
-	// if(arg[1].empty()){
-	// 	std::cout << "There must be 2 or 3 parameters for this command" << std::endl;
-	// 	return;
-	// }
-	// if(arg[0][0] == '#'){
-	// 	if(arg[1][0] != '+' && arg[1][0] != '-')
-	// 		std::cout << "Second parameter must begin with '+' or '-' (set/remove)" << std::endl;
-	// 	else if(arg[1][1] != 'i' && arg[1][1] != 't' && arg[1][1] != 'k' && arg[1][1] != 'o' && arg[1][1] != 'l')
-	// 		std::cout << "available modes : -i, -t, -k, -o, -l" << std::endl;
-	// 	else{
-	// 		removeFirstChar(arg, 0);
-	// 		// exec MODE
-	// 	}
-	// }
-	// else{
-	// 	std::cout << "First parameter must be a channel beginning with '#'" << std::endl;
-	// }
 	user.modeCmd(server, arg);
 }
 
@@ -151,38 +126,11 @@ void cmdPars::handleList(Server &server, User &user, std::vector<std::string> ar
 
 void cmdPars::handlePart(Server &server, User &user, std::vector<std::string> arg)
 {
-	// (void)user;
-	// (void)server;
-	// // PART (channel) [channel] ...
-	// if (arg[0].empty())
-	// {
-	// 	std::cout << "There must be at least 1 parameter for this command" << std::endl;
-	// 	return;
-	// }
-	// for (int i = 0; i < 15 && !arg[i].empty(); i++)
-	// {
-	// 	if (arg[i][0] != '#')
-	// 	{
-	// 		std::cout << "Channel name must begin with '#'" << std::endl;
-	// 		return;
-	// 	}
-	// 	removeFirstChar(arg, i);
-	// 	// partir du channel arg[i]
-	// 	std::cout << "[User] has left channel " << arg[i] << std::endl;
-	// }
 	user.partCmd(server,arg);
 }
 
 void cmdPars::handleNick(Server &server, User &user, std::vector<std::string> arg)
 {
-	(void)user;
-	// NICK (new nickname)
-	if (arg[0].empty())
-	{
-		std::cout << "There must be 1 parameter for this command" << std::endl;
-		return;
-	}
-	// changer nickname
 	user.nickCmd(server, arg);
 }
 
@@ -193,9 +141,6 @@ void cmdPars::handleQuit(Server &server, User &user, std::vector<std::string> ar
 
 void cmdPars::handleJoin(Server &server, User &user, std::vector<std::string> arg)
 {
-	(void)user;
-	// JOIN (channel) [mdp]
-	//  Server::messageToServer(arg[0].c_str(), NULL);
 	user.joinCmd(server, arg);
 }
 
